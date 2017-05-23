f956ca270162d014e755b08d6dc390db55ad574c
BertrandHustle <MFOrangeSlice@gmail.com>
2017-05-23 12:28:50 -0400

    repeat of last commit, forgot to add trebekbot.py

--- trebekbot.py
+++ trebekbot.py
@@ -4,29 +4,21 @@ import os
 import time
 from slackclient import SlackClient
 
-# retrieve id/token/etc. from env variables
-'''
-bot_id = os.environ.get('BOT_ID')
-slack_token = os.environ["SLACK_API_TOKEN"]
-'''
-
-
-
-# TODO: call this as env variable (see above), fix KeyError: SLACK_BOT_TOKEN
-slack_token = 'xoxb-183776207538-ksUMUx7Si6OrHYEEmltLW5ii'
-sc = SlackClient(slack_token)
 
 author = 'bertrand_hustle'
 bot_name = 'trebekbot'
 
+# retrieve id/token/etc. from env variables
+bot_id = os.environ.get('TREBEKBOT_ID')
+slack_token = os.environ['TREBEKBOT_API_TOKEN']
+
+sc = SlackClient(slack_token)
 sc.api_call(
     'chat.postMessage',
     channel='#trebektest',
-    text='Hi Lana?!'
+    text='poop'
 )
 print('test')
-
-
 
 '''
 if __name__ == "__main__":

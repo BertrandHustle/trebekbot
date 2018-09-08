098834e0ec20567984a1f4f2c107e7274b7de9dd
BertrandHustle <MFOrangeSlice@gmail.com>
2018-09-08 19:23:04 -0400

    fixed (hopefully) debug_daily_double()

--- host.py
+++ host.py
@@ -250,11 +250,12 @@ class Host:
 
     # asks daily double, but only if it's bertrand_hustle
     def debug_daily_double(self, slack_output):
-        user = self.get_user(slack_output)
-        if self.hear(slack_output, 'dd') and user == 'bertrand_hustle':
-            asked_question = question.Question(daily_double=True)
-            self.say(main.channel, asked_question.slack_text)
-            return asked_question
+        if self.hear(slack_output, 'dd'):
+            # this can't be an 'and' because we need valid slack output first
+            if self.get_user(slack_output) == 'bertrand_hustle':
+                asked_question = question.Question(daily_double=True)
+                self.say(main.channel, asked_question.slack_text)
+                return asked_question
 
     '''
     checks if the answer to a question is correct and updates score accordingly

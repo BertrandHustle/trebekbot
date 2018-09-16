a115916817fde94fe328f6a2d27c0b754c5d287d
BertrandHustle <MFOrangeSlice@gmail.com>
2018-09-16 16:38:39 -0400

    attempting to fix debug dd problem

--- main.py
+++ main.py
@@ -69,11 +69,10 @@ if __name__=='__main__':
         host.top_ten(slack_output)
         host.changelog(slack_output)
         host.say_uptime(slack_output)
-        host.crash(slack_output)
-
         # used for debugging purposes
-        # TODO: fix this so it only gives daily doubles
-        current_question = host.debug_daily_double(slack_output)
+        host.crash(slack_output)
+        # TODO impliment debug daily double here
+
         current_question = host.ask_question(slack_output)
 
         if current_question:

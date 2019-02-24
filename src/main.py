cb26fc85eb00bc5520045f44c0be2b06ce83c178
BertrandHustle <MFOrangeSlice@gmail.com>
2019-02-24 18:53:26 -0500

    typo fix

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ bot_name = 'trebekbot'
 debug = 0
 # setup database (or connect to existing one)
 # db_url = urlparse.urlparse(os.environ['DATABASE_URL']).path[1:]
-user_db = db.db(os.environ['DATABASE_URL'], sslmode=require)
+user_db = db.db(os.environ['DATABASE_URL'], sslmode='require')
 # create host object
 host = host.Host(slack_client, user_db)
 # retrieve id/token/etc. from env variables

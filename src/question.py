73a2863ed7cecf2c0b4418f6feb41f3139dfaa37
BertrandHustle <MFOrangeSlice@gmail.com>
2019-03-31 21:09:35 -0400

    fixing daily double flag

--- src/question.py
+++ src/question.py
@@ -46,7 +46,7 @@ class Question:
         # used to test daily doubles
         if daily_double:
             filtered_question_list = \
-            self.filter_questions(filtered_question_list, daily_double=0)
+            self.filter_questions(filtered_question_list)
         question = filtered_question_list[randint(0, len(filtered_question_list))]
         # text with html links separated out
         scrubbed_text = Question.separate_html(question['question'])

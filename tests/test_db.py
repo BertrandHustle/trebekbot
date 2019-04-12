84aa9d55435b03504c657be74cc762c5ea4a0922
BertrandHustle <MFOrangeSlice@gmail.com>
2019-04-11 21:03:36 -0400

    adding tests

---
+++ tests/test_db.py
@@ -0,0 +1,201 @@
+from sys import path as syspath
+syspath.append('c:\\users\\hooks\\documents\\programming\\projects\\trebekbot\\src')
+from re import findall
+from atexit import register
+from os import path, remove
+import pytest
+import db
+
+# set up test objects
+test_db = db.db('test.db')
+
+# kill db on exit
+def kill_test_db():
+    test_db.connection.close()
+    return remove(test_db.filepath)
+
+# fixture for tearing down test database completely
+@pytest.fixture
+def db_after():
+    yield db_after
+    test_db.drop_table_users(test_db.connection)
+
+# fixture for cleaning out test users from database (but leaving table present)
+# TODO: make this run after failed tests
+@pytest.fixture
+def scrub_test_users():
+    yield scrub_test_users
+    test_db.connection.execute(
+    '''
+    DELETE FROM USERS
+    '''
+    )
+
+# set up fixture to populate test db with a range of scorers
+@pytest.fixture
+def populate_db():
+    test_users = ['Bob', 'Jim', 'Carol', 'Eve', 'Morp']
+    test_score = 101
+    # ensure that we have a varied list of scorers
+    for user in test_users:
+        test_db.add_user_to_db(test_db.connection, user)
+        test_db.update_score(test_db.connection, user, test_score)
+        test_score += 100
+    # check to make sure negative numbers work too
+    test_db.connection.execute(
+    '''
+    UPDATE USERS SET SCORE = ? WHERE NAME = ?
+    ''',
+    (-156, 'Eve')
+    )
+    test_db.connection.execute(
+    '''
+    UPDATE USERS SET CHAMPION_SCORE = ? WHERE NAME = ?
+    ''',
+    (5000, 'Morp')
+    )
+
+# same as above, but all scores are zero
+@pytest.fixture
+def populate_db_all_scores_zero():
+    test_users = ['Bob', 'Jim', 'Carol', 'Eve', 'Morp']
+    for user in test_users:
+        test_db.add_user_to_db(test_db.connection, user)
+
+def test_add_user_to_db():
+    # do this twice to ensure that we're adhering to the UNIQUE constraint
+    test_db.add_user_to_db(test_db.connection, 'Bob')
+    test_db.add_user_to_db(test_db.connection, 'Bob')
+    test_query = test_db.connection.execute(
+    '''
+    SELECT * FROM USERS WHERE NAME = ?
+    ''',
+    ('Bob',)
+    )
+    query_results = test_query.fetchall()
+    check_results = findall(r'Bob', str(query_results))
+    # asserts both that Bob was added and that he was only added once
+    assert len(check_results) == 1
+
+@pytest.mark.parametrize("user, value_change, expected_result", [
+ ('LaVar', '0', 0),
+ ('LaVar', '-200', -200),
+ ('LaVar', '-200', -400),
+ ('LaVar', '500', 100),
+ ('Stemp', 'Invalid Value', 0),
+ ('boop', '-9000', -9000),
+ ('boop', '-500', -9500),
+ # this makes sense, if we get a non-int it should do nothing
+ ('LaVar', 'ants', 100)
+])
+def test_update_score(user, value_change, expected_result):
+    test_db.add_user_to_db(test_db.connection, user)
+    test_db.update_score(test_db.connection, user, value_change)
+    assert test_db.get_score(test_db.connection, user) == expected_result
+
+# we can't do this above because it scrubs the db after EVERY parametrized test
+scrub_test_users()
+
+# TODO: test force flag
+def test_return_top_ten(populate_db, scrub_test_users):
+    expected_list = [
+    (8, 'Morp', 501, 5000, 0, 0),
+    (6, 'Carol', 301, 0, 0, 0),
+    (5, 'Jim', 201, 0, 0, 0),
+    (1, 'Bob', 101, 0, 0, 0),
+    (2, 'LaVar', 100, 0, 0, 0),
+    (3, 'Stemp', 0, 0, 0, 0),
+    (7, 'Eve', -156, 0, 0, 0),
+    (4, 'boop', -9500, 0, 0, 0)
+    ]
+    assert test_db.return_top_ten(test_db.connection) == expected_list
+
+def test_increment_win(populate_db, scrub_test_users):
+    test_connection = test_db.connection
+    test_db.increment_win(test_connection, 'Carol')
+    test_query = test_connection.cursor().execute(
+    '''
+    SELECT WINS FROM USERS WHERE NAME = ?
+    ''', ('Carol',)
+    )
+    assert test_query.fetchall()[0][0] == 1
+
+def test_get_user_wins(populate_db, scrub_test_users):
+    test_connection = test_db.connection
+    test_db.increment_win(test_connection, 'Carol')
+    test_db.increment_win(test_connection, 'Carol')
+    test_db.increment_win(test_connection, 'Carol')
+    test_query = test_db.get_user_wins(test_connection, 'Carol')
+    assert test_query == 3
+
+def test_get_champion(populate_db, scrub_test_users):
+    expected_champion_name = 'Morp'
+    expected_champion_score = 501
+    assert test_db.get_champion(test_db.connection) == \
+    (expected_champion_name, expected_champion_score)
+    # create a tie
+    test_db.connection.execute(
+    '''
+    UPDATE USERS SET SCORE = ? WHERE NAME = ?
+    ''',
+    (10000, 'Bob')
+    )
+    test_db.connection.execute(
+    '''
+    UPDATE USERS SET SCORE = ? WHERE NAME = ?
+    ''',
+    (10000, 'Jim')
+    )
+    expected_champion_names = ['Bob', 'Jim']
+    expected_champion_scores = [10000, 10000]
+    assert test_db.get_champion(test_db.connection) == \
+    [(expected_champion_names[0], expected_champion_scores[0]),
+     (expected_champion_names[1], expected_champion_scores[1])]
+
+'''
+not a real unit test, but used to test the edge case where there is no champ
+for db.get_champion()
+'''
+def test_no_champion(populate_db_all_scores_zero, scrub_test_users):
+    assert test_db.get_champion(test_db.connection) == None
+
+def test_get_last_nights_champion(populate_db, scrub_test_users):
+    expected_champion_name = 'Morp'
+    expected_champion_score = 5000
+    assert test_db.get_last_nights_champion(test_db.connection) == \
+    (expected_champion_name, expected_champion_score)
+
+def test_set_champion(populate_db, scrub_test_users):
+    test_db.set_champion(test_db.connection, 'Morp', 501)
+    test_result = test_db.connection.execute(
+    '''
+    SELECT * FROM USERS WHERE CHAMPION = 1 AND CHAMPION_SCORE = 501
+    '''
+    ).fetchone()
+    assert (test_result[1], test_result[3]) == ('Morp', 501)
+
+# TODO: test if user doesn't exist
+def test_get_score(scrub_test_users):
+    test_db.add_user_to_db(test_db.connection, 'Lucy')
+    assert test_db.get_score(test_db.connection, 'Lucy') == 0
+    test_db.connection.execute(
+    '''
+    UPDATE USERS SET SCORE = ? WHERE NAME = ?
+    ''',
+    (100, 'Lucy')
+    )
+    assert test_db.get_score(test_db.connection, 'Lucy') == 100
+
+def test_wipe_scores(populate_db, db_after):
+    test_db.wipe_scores(test_db.connection)
+    # get scores
+    test_scores = test_db.connection.execute(
+    '''
+    SELECT * FROM USERS ORDER BY SCORE DESC
+    ''',
+    ).fetchall()
+    test_scores = [x[2] for x in test_scores]
+    # this works because every score should be 0
+    assert not any(test_scores)
+
+register(kill_test_db)
---
+++ tests/test_host.py
@@ -0,0 +1,225 @@
+import os
+from sys import path as syspath
+syspath.append(
+os.path.abspath(
+os.path.join(
+os.path.dirname(__file__), os.path.pardir)))
+import src.host as host
+import src.db as db
+import pytest
+import json
+import slackclient
+
+# set up test objects
+sc = slackclient.SlackClient('slack_token')
+
+# set up test db
+test_db = db.db('test.db')
+
+# TODO: find a way to make this not duplicate code from test_db
+def populate_db(database):
+    test_users = ['Bob', 'Jim', 'Carol', 'Eve', 'Morp']
+    test_score = 101
+    # ensure that we have a varied list of scorers
+    for user in test_users:
+        database.add_user_to_db(database.connection, user)
+        database.update_score(database.connection, user, test_score)
+        test_score += 100
+    test_cursor = database.connection.cursor()
+    # check to make sure negative numbers work too
+    test_cursor.execute(
+    '''
+    UPDATE USERS SET SCORE = ? WHERE NAME = ?
+    ''',
+    (-156, 'Eve')
+    )
+    test_cursor.execute(
+    '''
+    UPDATE USERS SET CHAMPION_SCORE = ? WHERE NAME = ?
+    ''',
+    (5000, 'Morp')
+    )
+
+populate_db(test_db)
+test_host = host.Host(sc)
+
+def test_get_channel_id():
+    fail_test_json = {"ok": False, "error": "invalid_auth"}
+    test_json = json.load(open(path.join('support_files', 'json', 'test_get_channel_id.json')))
+    expected_id = 'C600FK4T1'
+    assert test_host.get_channel_id_from_json('trivia', test_json) == expected_id
+    assert test_host.get_channel_id_from_json('trivia', fail_test_json) == None
+    assert test_host.get_channel_id_from_json('trivia', None) == None
+
+def test_get_bot_id():
+    expected_id = "0"
+    assert test_host.get_bot_id('trebekbot') == expected_id
+
+@pytest.mark.parametrize("wager, user_score, expected_value", [
+ ('aw, he restarted', 1500, None),
+ (500, 500, 500),
+ ('bees', 1500, None),
+ # this will prompt trebekbot to re-ask for a wager
+ (0, 0, None),
+ (-500, 2500, 0),
+ # test jeopardy minimum of 1000 rule
+ (1000, 100, 1000),
+ (10000, 100, 1000)
+])
+def test_calc_wager(wager, user_score, expected_value):
+    assert test_host.calc_wager(wager, user_score) == expected_value
+
+@pytest.mark.parametrize("test_value, expected_value", [
+ # we add an extra space because this is how we get the answers from slack
+ (' Hall & Oates', ['hall', 'oates']),
+ (' Hall and Oates', ['hall', 'oates']),
+ (' Androgynous', ['androgynous']),
+ (' Hall & Oates\' Oats and Halls', ['hall', 'oates', 'oats', 'halls']),
+ ('the absolute density of a dying star', \
+ ['absolute', 'density', 'dying', 'star']),
+ (' "The Little Rascals"', ['little', 'rascals']),
+ ('(the) Dow (Jones)', ['dow']),
+ ('S-P-E-L-L-I-N-G', ['spelling']),
+ # test extra whitespace
+ (' S-P-E-L-L-I-N-G', ['spelling'])
+])
+def test_strip_answer(test_value, expected_value):
+    assert test_host.strip_answer(test_value) == expected_value
+
+@pytest.mark.parametrize("given_answer, expected_answer, expected_list", [
+ ('Bath' , 'Borth', [('bath', 'borth')]),
+ ('the dover cliffs', 'The White Cliffs of Dover', \
+ [('dover', 'white'), ('dover', 'cliffs'), ('cliffs', 'white'), \
+ ('dover', 'dover'), ('cliffs', 'cliffs')]),
+ ('the dover cliffs', 'the cover dliffs', \
+ [('dover', 'cover'), ('dover', 'dliffs'), ('cliffs', 'dliffs'), ('cliffs', 'cover')]),
+ ('mary queen of scotts', 'Mary, Queen of Scots', \
+ [('mary', 'mary'), ('mary', 'queen'), ('mary', 'scots'), \
+ ('queen', 'queen'), ('queen', 'scots'), \
+ ('scotts', 'mary'), ('scotts', 'queen'), ('scotts', 'scots')],
+ )
+])
+def test_pair_off_answers(given_answer, expected_answer, expected_list):
+  # it's assumed that we'll be doing this first in our regular answer checking
+  given_answer = test_host.strip_answer(given_answer)
+  expected_answer = test_host.strip_answer(expected_answer)
+  # convert to set so we don't need to worry about ordering
+  test_list = set(test_host.pair_off_answers(given_answer, expected_answer))
+  expected_list = set(expected_list)
+  assert test_list == expected_list
+
+@pytest.mark.parametrize("given_word, expected_word, expected_value", [
+  ('Test', 'Toast', False),
+  ('Test', 'Tost', True),
+  ('Test', 'te', False),
+  ('bethlehem', 'bethelhem', 'close'),
+  ('t', 'testaholic', False),
+  ('Mr. Rodney', 'Mr. Rogers', False),
+  ('Mr. Rodgers', 'Mr. Rogers', True),
+  ('Mr. Roerss', 'Mr. Rogers', 'close')
+])
+def test_fuzz_word(given_word, expected_word, expected_value):
+    assert test_host.fuzz_word(given_word, expected_word) == expected_value
+
+# TODO: test this for crashing
+'''
+category": "MYTHOLOGICAL CROSSWORD CLUES \"M\"", "air_date": "1998-10-13",
+"question": "'\"Touch\"y golden guy<br />             (5)'",
+"value": "$200", "answer": "King Midas", "round": "Double Jeopardy!",
+"show_number": "3242"}
+'''
+
+@pytest.mark.parametrize("given_answer, expected_answer, expected_value", [
+ ('Bath', 'Borth', False),
+ ('Bath', 'beth', True),
+ (None, 'Borth', False),
+ ('mary queen of scotts','Mary, Queen of Scots', True),
+ ('','Mary, Queen of Scots', False),
+ ('MAAAARYYYY QUEEN OF SCOOOOOOTTSSS','Mary, Queen of Scots', False),
+ ('borp', 'Henry James', False),
+ ('bagpipe', 'a bagpipe', True),
+ ('amber alert', 'an Amber alert', True),
+ ('infintesimal', 'infinitesimal', True),
+ ('infiniitesimal', 'infinitesimal', True),
+ ('the good Samaritan', 'The Good Samaritan', True),
+ ('it’s a wonderful life', 'It\'s A Wonderful Life', True),
+ ('Hall and Oates', 'Hall & Oates', True),
+ ('b', 'the Boston Massacre', False),
+ ('omlette', 'Denver omelette', 'close'),
+ ('The Great Star of Bethlehem', 'The Star of Bethelhem', 'close'),
+ ('lawn', 'The Great Lawn', 'close'),
+ ('bechamel', 'béchamel', True),
+ ('queen elizabeth ii', 'Elizabeth II', True),
+ ('issac newton', 'Newton', 'close'),
+ # test optional parentheses
+ ('the dow jones', '(the) Dow (Jones)', True),
+ ('dow', '(the) Dow (Jones)', True),
+ ('Red and Green', 'Green and Red', True),
+ ('Blue or green', 'Green', True),
+ ('poker', 'a poker face', 'close'),
+ ('the gay 90\'s', 'The Gay \'90s', True),
+ ('REM', 'R.E.M.', True),
+ ('hard days night', '"A Hard Day\'s Night"', True),
+ ('HG Wells', '(H.G.) Wells', True),
+ ('cat\'s in the cradle', 'Cats In The Cradle', True),
+ ('Zermelo Frankel set theory', 'Zermelo-Frankel Set Theory', True),
+ ('00', '00', True),
+ ('91', '21', False),
+ ('32', '32', True),
+ ('the absolute density of a dying star', 'star', False),
+ ('star', 'the absolute density of a dying star', False),
+ ('world war 2', 'World War II', True),
+ # special case for spelling bee questions, may need to filter those out
+ ('Rorschach', 'R-O-R-S-C-H-A-C-H', True),
+ ('R&D', 'R&D', True),
+ ('L', 'Y', False),
+ ('8', '4 pounds', False),
+ # make sure we arent' too lenient for longer words
+ ('featherweight', 'welterweight', False),
+ # slashes
+ ('istanbul', 'istanbul/constantinople', True)
+])
+def test_fuzz_answer(given_answer, expected_answer, expected_value):
+    assert test_host.fuzz_answer(given_answer, expected_answer) == expected_value
+
+@pytest.mark.parametrize("given_answer, correct_answer, expected_reply", [
+ ('princess di', 'Princess Diana', 'Please be more specific.'),
+ ('general mills and general electric', 'General Motors & General Electric', \
+ 'Please be more specific.'),
+ ('the almighty king of siam', 'the king of siam', 'Please be less specific.'),
+ ('corporal klinger', 'max klinger', 'Please be more specific.')
+])
+def test_check_closeness(given_answer, correct_answer, expected_reply):
+    assert test_host.check_closeness(given_answer, correct_answer) == \
+    expected_reply
+
+@pytest.mark.parametrize("slack_output, user_score, expected_value", [
+ ([{'source_team': 'T0LR9NXQQ', 'team': 'T0LR9NXQQ', 'text':
+ '..wager 20000', 'type': 'message', 'ts': '1497097067.238474',
+ 'user': 'U1UU5ARJ6', 'channel': 'C5LMQHV5W'}], -5000, 1000),
+ ([{'source_team': 'T0LR9NXQQ', 'team': 'T0LR9NXQQ', 'text':
+ '..wager 10000', 'type': 'message', 'ts': '1497097067.238474',
+ 'user': 'U1UU5ARJ6', 'channel': 'C5LMQHV5W'}], 10000, 10000),
+ ([{'source_team': 'T0LR9NXQQ', 'team': 'T0LR9NXQQ', 'text':
+ '..wager 0', 'type': 'message', 'ts': '1497097067.238474',
+ 'user': 'U1UU5ARJ6', 'channel': 'C5LMQHV5W'}], 10000, None)
+])
+def test_get_wager(slack_output, user_score, expected_value):
+    assert test_host.get_wager(slack_output, user_score) == expected_value
+
+def test_get_latest_changelog():
+    changelog = 'support_files/test_README.md'
+    test_latest_changelog = ['version 0.5.3 changelog (4-5-18):',
+    '',
+    'Bugs:',
+    '  - fixed crash from querying slack api for channel',
+    "  - fixed issue with 'spelling bee' questions",
+    '  - parentheses in answers now treated as optional to the answer',
+    '',
+    'Features:',
+    '  - trebekbot now gives a top ten list of scorers before nightly restarts',
+    '  - increased daily double timer to 90 seconds',
+    '']
+    print(test_host.get_latest_changelog(changelog).split('\n'))
+    assert test_host.get_latest_changelog(changelog).split('\n')[:-1] \
+    == test_latest_changelog

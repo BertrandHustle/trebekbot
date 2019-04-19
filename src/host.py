fc6065e19bc683453d1c9aa07135540d55d9409b
BertrandHustle <MFOrangeSlice@gmail.com>
2019-04-18 21:54:42 -0400

    still fixing champion behavior

--- src/host.py
+++ src/host.py
@@ -55,14 +55,12 @@ class Host:
         # connect to user database
         self.user_db = user_db
         # get current champion info
-        self.last_night_champion = self.user_db.get_last_nights_champion(self.user_db.connection)
-        '''
+        self.current_champion_score, self.current_champion_name = None, None
         try:
             self.current_champion_score, self.current_champion_name = \
             self.user_db.get_last_nights_champion(self.user_db.connection)
         except TypeError:
             pass
-        '''
 
     # listens for output in slack channel
     '''

6a22fa35b9565792a2fc6156b1351d68b771f62a
BertrandHustle <scott.greenberg926@gmail.com>
2020-05-21 15:41:58 -0400

    adding sleep to hello route after handler

--- src/main.py
+++ src/main.py
@@ -146,15 +146,19 @@ live_question = Question(Question.get_random_question(), Timer(time_limit, reset
 
 
 # Routes
+def hello_handler():
+    payload = jsonify({'text': 'TEST'})
+    payload.status_code = 200
+    post(os.environ['WEBHOOK'], json=payload)
 
 # say hi!
 @app.route('/hello', methods=['POST'])
 def hello():
     # TEST
     if request.form['channel_name'] == channel:
-        payload = jsonify({'text': 'TEST'})
-        payload.status_code = 200
-        post(os.environ['WEBHOOK'], json=payload)
+        Thread(target=hello_handler)
+        from time import sleep
+        sleep(1)
         user_name = request.form['user_name']
         user_id = request.form['user_id']
         payload = {

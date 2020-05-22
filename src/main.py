04968d44b4dc8efe093cf7e4a3584827c9b99efb
BertrandHustle <scott.greenberg926@gmail.com>
2020-05-22 16:06:57 -0400

    changing return val in hello to Response object

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ from src.host import Host
 from src.judge import Judge
 from src.question import Question
 # third-party libs
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request, Response
 from requests import post
 from slack import WebClient
 
@@ -176,7 +176,7 @@ def hello():
         response_url = request.form['response_url']
         Thread(target=rev_hello_handler, args=[response_url, user_name, user_id]).start()
         with app.app_context():
-            return make_response(200)
+            return Response(status=200)
     else:
         return handle_payload(wrong_channel_payload)
 

82ea5a993e47e239805a0a10a79c49cf5e2dc687
BertrandHustle <scott.greenberg926@gmail.com>
2020-06-10 12:22:52 -0400

    added db test to check for scores below -10000

--- tests/test_db.py
+++ tests/test_db.py
@@ -51,6 +51,7 @@ def populate_db_all_scores_zero():
     for user in test_users:
         test_db.add_user_to_db(test_db.connection, user)
 
+
 def test_add_user_to_db():
     # do this twice to ensure that we're adhering to the UNIQUE constraint
     test_db.add_user_to_db(test_db.connection, 'Bob')
@@ -67,6 +68,7 @@ def test_add_user_to_db():
     assert len(check_results) == 1
     assert type(query_results[0][0]) == int
 
+
 @pytest.mark.parametrize("user, value_change, expected_result", [
  ('LaVar', '0', 0),
  ('LaVar', '-200', -200),
@@ -75,6 +77,8 @@ def test_add_user_to_db():
  ('Stemp', 'Invalid Value', 0),
  ('boop', '-9000', -9000),
  ('boop', '-500', -9500),
+ # the min score is -10000 to prevent players from going too far into the hole
+ ('boop', '-1000', -10000),
  # this makes sense, if we get a non-int it should do nothing
  ('LaVar', 'ants', 100)
 ])
@@ -83,8 +87,12 @@ def test_update_score(user, value_change, expected_result):
     test_db.update_score(test_db.connection, user, value_change)
     assert test_db.get_score(test_db.connection, user) == expected_result
 
+
 # we can't do this above because it scrubs the db after EVERY parametrized test
-scrub_test_users()
+# scrub_test_users()
+
+test_cursor.execute('DELETE FROM USERS')
+
 
 # TODO: test force flag
 def test_return_top_ten(populate_db, scrub_test_users):
@@ -104,6 +112,7 @@ def test_return_top_ten(populate_db, scrub_test_users):
     assert len(test_top_ten) == 4
     assert top_ten_names == expected_names
 
+
 def test_increment_win(populate_db, scrub_test_users):
     test_db.increment_win(test_db.connection, 'Carol')
     test_cursor.execute(
@@ -113,6 +122,7 @@ def test_increment_win(populate_db, scrub_test_users):
     )
     assert test_cursor.fetchall()[0][0] == 1
 
+
 def test_get_user_wins(populate_db, scrub_test_users):
     test_connection = test_db.connection
     test_db.increment_win(test_connection, 'Carol')
@@ -120,6 +130,7 @@ def test_get_user_wins(populate_db, scrub_test_users):
     test_db.increment_win(test_connection, 'Carol')
     test_query = test_db.get_user_wins(test_connection, 'Carol')
     assert test_query == 3
+
 
 def test_get_champion(populate_db, scrub_test_users):
     expected_champion_name = 'Morp'
@@ -145,12 +156,14 @@ def test_get_champion(populate_db, scrub_test_users):
     [(expected_champion_names[0], expected_champion_scores[0]),
      (expected_champion_names[1], expected_champion_scores[1])]
 
+
 '''
 not a real unit test, but used to test the edge case where there is no champ
 for db.get_champion()
 '''
 def test_no_champion(scrub_test_users):
     assert test_db.get_champion(test_db.connection) == None
+
 
 # TODO: test if user doesn't exist
 def test_get_score(scrub_test_users):
@@ -163,6 +176,7 @@ def test_get_score(scrub_test_users):
     (100, 'Lucy')
     )
     assert test_db.get_score(test_db.connection, 'Lucy') == 100
+
 
 def test_wipe_scores(populate_db):
     test_db.wipe_scores(test_db.connection)

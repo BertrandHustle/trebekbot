4dff0473129e7e6cad70e170894c066e10db2ce0
BertrandHustle <scott.greenberg926@gmail.com>
2021-03-18 19:53:32 -0400

    init commit, database is working

---
+++ game/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    score = models.IntegerField(default=0)
+    wins = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=750)
+    value = models.IntegerField()
+    category = models.CharField(max_length=100)
+    daily_double = models.BooleanField(default=False)
+    answer = models.CharField(max_length=250)
+    date = models.CharField(max_length=50)
+    valid_links = models.CharField(max_length=500, blank=True, default='')
+
+    def __str__(self):
+        return f'{self.category} | {self.value} | {self.date} | {self.text}'

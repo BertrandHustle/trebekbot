f1b1c4abc768e301ba5f6e1d46f0909f94338cdd
BertrandHustle <scott.greenberg926@gmail.com>
2021-04-08 20:17:41 -0400

    create account page is reachable

--- game/urls.py
+++ game/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from game.views import game_views, account_views
+from game.views import game_views, registration_views
 
 urlpatterns = [
     path('', game_views.index, name='index'),
     # game
     path('play', game_views.play, name='play'),
     # registration
-    path('create_account', account_views.create_account, name='create_account'),
+    path('create_account', registration_views.create_account, name='create_account'),
 ]
 

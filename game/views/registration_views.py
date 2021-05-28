5fd5062c96d04905d033d88c8eb683f16728c339
BertrandHustle <scott.greenberg926@gmail.com>
2021-05-27 21:02:08 -0400

    new users now linked to player objects

--- game/views/registration_views.py
+++ game/views/registration_views.py
@@ -1,22 +1,33 @@
-from django.contrib.auth import login as django_login, authenticate
+from django.contrib.auth import login as django_login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import redirect
 from django.shortcuts import render
+from game.models import Player
 
 
 def login(request):
     return render(request, "registration/login.html")
 
 
-#TODO: make users re-sign in after page is closed
+#TODO: implement as logout button on every page
+def logout_view(request):
+    logout(request)
+
+
+#TODO: fix CSRF token error when this page is accessed by logged-in user
+#TODO: make users re-sign in after page is closed in their browser
 def create_account(request):
     new_user_form = UserCreationForm(request.POST)
     if new_user_form.is_valid():
-        new_user_form.save()
-        username = new_user_form.cleaned_data.get('username')
-        password = new_user_form.cleaned_data.get('password')
-        user = authenticate(username=username, password=password)
+        user = new_user_form.save()
         django_login(request, user)
+        # # get new user object
+        username = new_user_form.cleaned_data.get('username')
+        newly_created_user = User.objects.get(username=username)
+        # # link and create new Player object
+        player = Player(user=newly_created_user, name=username)
+        player.save()
         return redirect('index')
     return render(request, 'game/create_account.html', {'form': new_user_form})
 

dba9069fda43dfa862d5c3e23299ea56d295784e
BertrandHustle <scott.greenberg926@gmail.com>
2022-04-14 19:40:12 -0400

    updated app to wipe scores on startup

--- game/apps.py
+++ game/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class GameConfig(AppConfig):
     name = 'game'
+
+    # wipe scores on startup
+    def ready(self):
+        from .models import Player
+        Player.objects.all().update(score=0)

ed1b44517a2d8e082429e0c2bc9d89ff5917500c
BertrandHustle <scott.greenberg926@gmail.com>
2023-10-05 19:36:46 -0400

    replaced python-dotenv with django-dotenv

--- trebekbot/settings/prod.py
+++ trebekbot/settings/prod.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import dj_database_url
 import os
+
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

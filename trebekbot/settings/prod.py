affc8649eaa005f73c20c949fac580dc3eef2e08
BertrandHustle <scott.greenberg926@gmail.com>
2023-10-19 20:53:46 -0400

    add generic/wildcard hosts to ALLOWED_HOSTS for testing in prod settings

--- trebekbot/settings/prod.py
+++ trebekbot/settings/prod.py
@@ -28,10 +28,14 @@ SECRET_KEY = 'kuc%wnd&z6t2r$2e1limsa*4$_049kpipz0=8$p$sp^41%c7ex'
 DEBUG = False
 
 # CORS/CSRF
-ALLOWED_HOSTS = ['https://trebekbot-react-200b3046b53e.herokuapp.com']
+ALLOWED_HOSTS = [
+    'https://trebekbot-react-200b3046b53e.herokuapp.com',
+    'trebekbot-react.herokuapp.com',
+    'trebekbot-react*.herokuapp.com'
+]
 # CORS_ORIGIN_ALLOW_ALL = False
-CORS_ALLOWED_ORIGINS = ['https://trebekbot-react-200b3046b53e.herokuapp.com']
-CSRF_TRUSTED_ORIGINS = ['https://trebekbot-react-200b3046b53e.herokuapp.com']
+CORS_ALLOWED_ORIGINS = ALLOWED_HOSTS
+CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
 
 # CORS_EXPOSE_HEADERS = ["Content-Type", "X-CSRFToken"]
 CORS_ALLOW_CREDENTIALS = True

ec8987f1a45abf66ccb09e11eb700e610916358f
BertrandHustle <scott.greenberg926@gmail.com>
2023-10-26 20:35:31 -0400

    set allowed hosts to all

--- trebekbot/settings/prod.py
+++ trebekbot/settings/prod.py
@@ -28,12 +28,13 @@ SECRET_KEY = 'kuc%wnd&z6t2r$2e1limsa*4$_049kpipz0=8$p$sp^41%c7ex'
 DEBUG = False
 
 # CORS/CSRF
-ALLOWED_HOSTS = [
-    '.herokuapp.com'
-    'trebekbot-python-8faaf61138b9.herokuapp.com',
-    'trebekbot-python.herokuapp.com',
-    'trebekbot-python*.herokuapp.com'
-]
+# ALLOWED_HOSTS = [
+#     '.herokuapp.com'
+#     'trebekbot-python-8faaf61138b9.herokuapp.com',
+#     'trebekbot-python.herokuapp.com',
+#     'trebekbot-python*.herokuapp.com'
+# ]
+ALLOWED_HOSTS = ['*']
 # # CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOWED_ORIGINS = CSRF_TRUSTED_ORIGINS = [
     'https://trebekbot-react-200b3046b53e.herokuapp.com',

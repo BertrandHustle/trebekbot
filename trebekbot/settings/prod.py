0e36340b374375faba8a69b219cbd61efdb561a1
BertrandHustle <scott.greenberg926@gmail.com>
2023-10-26 20:32:21 -0400

    fix allowed hosts not being string

--- trebekbot/settings/prod.py
+++ trebekbot/settings/prod.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'kuc%wnd&z6t2r$2e1limsa*4$_049kpipz0=8$p$sp^41%c7ex'
 DEBUG = False
 
 # CORS/CSRF
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 # # CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOWED_ORIGINS = CSRF_TRUSTED_ORIGINS = [
     'https://trebekbot-react-200b3046b53e.herokuapp.com',

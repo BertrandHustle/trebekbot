9e0851dd807a5ec17c0856564b789fff70d394e2
BertrandHustle <scott.greenberg926@gmail.com>
2023-10-26 20:32:57 -0400

    remove blank allowed_hosts var

--- trebekbot/settings/prod.py
+++ trebekbot/settings/prod.py
@@ -28,14 +28,18 @@ SECRET_KEY = 'kuc%wnd&z6t2r$2e1limsa*4$_049kpipz0=8$p$sp^41%c7ex'
 DEBUG = False
 
 # CORS/CSRF
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '.herokuapp.com'
+    'trebekbot-python-8faaf61138b9.herokuapp.com',
+    'trebekbot-python.herokuapp.com',
+    'trebekbot-python*.herokuapp.com'
+]
 # # CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOWED_ORIGINS = CSRF_TRUSTED_ORIGINS = [
     'https://trebekbot-react-200b3046b53e.herokuapp.com',
     'https://trebekbot-react.herokuapp.com',
     'https://trebekbot-react*.herokuapp.com'
 ]
-ALLOWED_HOSTS = []
 # CORS_EXPOSE_HEADERS = ["Content-Type", "X-CSRFToken"]
 CORS_ALLOW_CREDENTIALS = True
 

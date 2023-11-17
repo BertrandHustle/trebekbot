1e0ace5990568ee5818f3125396d02e74732bf5a
BertrandHustle <scott.greenberg926@gmail.com>
2023-11-16 19:40:47 -0500

    remove unused logging import from auth_views; generate random secret key in prod settings

--- game/views/auth_views.py
+++ game/views/auth_views.py
@@ -1,5 +1,4 @@
 import base64
-import logging
 
 from django.contrib.auth import authenticate, login, logout
 from django.utils.decorators import method_decorator
--- trebekbot/settings/prod.py
+++ trebekbot/settings/prod.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from django.core.management.utils import get_random_secret_key
+
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -22,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kuc%wnd&z6t2r$2e1limsa*4$_049kpipz0=8$p$sp^41%c7ex'
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

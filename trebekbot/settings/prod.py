92096d4a0e796de7caf62c57d467d9143ffae584
BertrandHustle <scott.greenberg926@gmail.com>
2024-01-11 21:21:51 -0500

    remove isAuth default permission

--- trebekbot/settings/prod.py
+++ trebekbot/settings/prod.py
@@ -59,9 +59,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ],

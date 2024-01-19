cb66598b909ebc8e2aadbde54687a0ccef68c7d4
BertrandHustle <scott.greenberg926@gmail.com>
2024-01-18 21:31:12 -0500

    remove basic auth from settings

--- trebekbot/settings/prod.py
+++ trebekbot/settings/prod.py
@@ -58,7 +58,6 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',

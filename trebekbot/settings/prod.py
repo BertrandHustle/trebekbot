14a47f4b4b52ac6a9ec6ea79f7bd364675f8b6b9
BertrandHustle <scott.greenberg926@gmail.com>
2024-01-25 20:45:12 -0500

    move secret key to heroku vars

--- trebekbot/settings/prod.py
+++ trebekbot/settings/prod.py
@@ -12,9 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-from django.core.management.utils import get_random_secret_key
-
 import dj_database_url
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_random_secret_key()
+# SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # TODO: turn this back off!

5b934f17edb383dbf0023cdcb0e5d29392e0781c
BertrandHustle <scott.greenberg926@gmail.com>
2024-02-29 19:57:11 -0500

    explicitly set Partitioned attr on csrf/session cookies in middleware

--- trebekbot/middleware.py
+++ trebekbot/middleware.py
@@ -2,23 +2,20 @@
 
 from http import cookies
 
-from django.conf import settings
 from django.http.request import HttpRequest
 from django.http.response import HttpResponseBase
 from django.middleware.http import MiddlewareMixin
 
-cookies.Morsel._flags.add("partitioned")
-cookies.Morsel._reserved.setdefault("partitioned", "Partitioned")
+cookies.Morsel._flags.add('partitioned')
+cookies.Morsel._reserved.setdefault('partitioned', 'Partitioned')
 
 
 class CookiePartitioningMiddleware(MiddlewareMixin):
     def process_response(self, request: HttpRequest, response: HttpResponseBase) -> HttpResponseBase:
-        for name in (
-            getattr(settings, f"{prefix}_COOKIE_NAME")
-            for prefix in ("CSRF", "SESSION", "LANGUAGE")
-            if getattr(settings, f"{prefix}_COOKIE_SECURE")
-        ):
-            if cookie := response.cookies.get(name):
-                cookie["Partitioned"] = True
-
+        csrf_cookie = response.cookies.get('CSRF_COOKIE_NAME')
+        session_id_cookie = response.cookies.get('SESSION_COOKIE_NAME')
+        if csrf_cookie:
+            csrf_cookie['Partitioned'] = True
+        if session_id_cookie:
+            session_id_cookie['Partitioned'] = True
         return response

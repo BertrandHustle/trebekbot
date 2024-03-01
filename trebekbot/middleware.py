747b22738c5979a8b5b28c566035149a6dc6af5a
BertrandHustle <scott.greenberg926@gmail.com>
2024-02-29 20:02:19 -0500

    pull cookie names from django settings

--- trebekbot/middleware.py
+++ trebekbot/middleware.py
@@ -2,6 +2,7 @@
 
 from http import cookies
 
+from django.conf import settings
 from django.http.request import HttpRequest
 from django.http.response import HttpResponseBase
 from django.middleware.http import MiddlewareMixin
@@ -12,8 +13,10 @@ cookies.Morsel._reserved.setdefault('partitioned', 'Partitioned')
 
 class CookiePartitioningMiddleware(MiddlewareMixin):
     def process_response(self, request: HttpRequest, response: HttpResponseBase) -> HttpResponseBase:
-        csrf_cookie = response.cookies.get('CSRF_COOKIE_NAME')
-        session_id_cookie = response.cookies.get('SESSION_COOKIE_NAME')
+        csrf_cookie_name = getattr(settings, 'CSRF_COOKIE_NAME')
+        session_id_name = getattr(settings, 'SESSION_COOKIE_NAME')
+        csrf_cookie = response.cookies.get(csrf_cookie_name)
+        session_id_cookie = response.cookies.get(session_id_name)
         if csrf_cookie:
             csrf_cookie['Partitioned'] = True
         if session_id_cookie:
